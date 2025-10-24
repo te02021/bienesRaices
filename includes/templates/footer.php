b8782579b5657041de430a8e4dbf3687ff716326
te02021 <teoorivero15mail.com>
2025-10-24 18:16:46 -0300

    primer commit

---
+++ includes/templates/footer.php
@@ -0,0 +1,14 @@
+<footer class="footer seccion">
+    <div class="contenedor contenedor-footer">
+        <nav class="navegacion">
+            <a href="nosotros.php">Nosotros</a>
+            <a href="anuncios.php">Anuncios</a>
+            <a href="blog.php">Blog</a>
+            <a href="contacto.php">Contacto</a>
+        </nav>
+    </div>
+    <?php
+        $fecha = date('Y');
+    ?>
+    <p class="copyright">Todos los derechos Reservados <?php echo $fecha?> &copy;</p>
+</footer>

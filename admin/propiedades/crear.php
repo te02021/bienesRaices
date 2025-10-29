2a9b4aa60530bc416d61305b8152b4045a9577fe
te02021 <teoorivero15@gmail.com>
2025-10-29 20:26:00 -0300

    310 listo

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -1,5 +1,13 @@
 <?php
+    require '../../includes/config/database.php';
     require '../../includes/funciones.php';
+    conectarDB();
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        echo '<pre>';
+        var_dump($_POST);
+        echo '</pre>';
+    }
     incluirTemplate('header');
 ?>
 
@@ -8,12 +16,43 @@
 
         <a class="boton-verde" href="/admin">Volver</a>
 
-        <form class="formulario">
+        <form class="formulario" method="POST" action="/admin/propiedades/crear.php">
             <fieldset>
                 <legend>Informacion General</legend>
                 <label for="titulo">Titulo</label>
-                <input type="text" id="titulo" placeholder="Ingresa el titulo de la propiedad"></input>
-            </fielset>
+                <input type="text" id="titulo" name="titulo" placeholder="Ingresa el titulo de la propiedad"></input>
+
+                <label for="precio">Precio</label>
+                <input type="number" id="precio" name="precio" placeholder="Ingrese el precio de la propiedad"></input>
+
+                <label for="imagen">Imagen</label>
+                <input type="file" id="imagen" name="imagen" accept="image/jpeg, image/png"></input>
+
+                <label for="descripcion">Descripcion</label>
+                <textarea id="descripcion"></textarea>
+            </fieldset>
+
+            <fieldset>
+                <legend>Informacion Propiedad</legend>
+                <label for="habitaciones">Habitaciones</label>
+                <input type="number" id="habitaciones" placeholder="Ej: 3" min=1 max=10></input>
+
+                <label for="wc">Baños</label>
+                <input type="number" id="wc" placeholder="Ej: 2" min=1 max=10></input>
+
+                <label for="estacionamientos">Estacionamientos</label>
+                <input type="number" id="estacionamientos" placeholder="Ej: 4" min=1 max=10></input>
+            </fieldset>
+
+            <fieldset>
+                <legend>Vendedor</legend>
+                <select>
+                    <option value="1">Mateo</option>
+                    <option value="2">Melody</option>
+                </select>
+            </fieldset>
+
+            <input type="submit" value="Crear propiedad" class="boton-verde"/>
         </form>    
 
     </main>
